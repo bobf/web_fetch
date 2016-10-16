65b2139d9b5c2b02b1f6c23b86e86fadad256356
Bob Farrell <robertanthonyfarrell@gmail.com>
2016-10-16 12:04:46 +0100

    Add binary, enable client to spawn server

--- lib/web_fetch/client.rb
+++ lib/web_fetch/client.rb
@@ -1,12 +1,34 @@
 module WebFetch
   class Client
-    def initialize(host, port)
+    def initialize(host, port, options = {})
       @host = host
       @port = port
+      @process = options[:process]
+    end
+
+    def self.create(host, port, options = {})
+      # Will block until process is responsive
+      process = spawn(host, port, options)
+      client = new(host, port, process: process)
+      while not client.alive?
+        sleep 0.1
+      end
+      client
+    end
+
+    def stop
+      # Will block until process dies
+      return if @process.nil?
+      @process.stop
+      @process.wait
     end
 
     def alive?
-      response = get('/')
+      begin
+        response = get('/')
+      rescue Errno::ECONNREFUSED
+        return false
+      end
       response.code == 200 && response.body == 'WebFetch'
     end
 
@@ -24,6 +46,17 @@ module WebFetch
         symbolize(response.body)
       elsif response.code == 404
         nil
+      end
+    end
+
+    class << self
+      def spawn(host, port, options)
+        path = options.fetch(:path, 'bin/web_fetch_server')
+        process = ChildProcess.build(path, '--host', host, '--port', "#{port}")
+        process.environment['RUBYLIB'] = 'lib/'
+        process.io.inherit!
+        process.start
+        process
       end
     end
 
