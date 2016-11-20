8528059223bbe1cb0c9bb812bc23c7ddec5781b0
Bob Farrell <robertanthonyfarrell@gmail.com>
2016-11-20 14:03:12 +0000

    Replace Unirest with Faraday
    Unirest was doing some crazy stuff with post data. Swapping out for Faraday
    removed the issue and generally gives better control over how request/response
    lifecycles are processed.

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -1,5 +1,3 @@
-require 'unirest'
-
 describe WebFetch::Server do
   let(:port) { 8089 }
   let(:host) { 'localhost' }
@@ -7,8 +5,8 @@ describe WebFetch::Server do
 
   it 'accepts HTTP connections' do
     response = get(host_uri)
-    expect(response.code).to eql 200
-    expect(response.body['application']).to eql 'WebFetch'
+    expect(response.success?).to be true
+    expect(JSON.parse(response.body)['application']).to eql 'WebFetch'
   end
 
   describe '/gather' do
@@ -19,13 +17,14 @@ describe WebFetch::Server do
 
     it 'responds "Unprocessable Entity" when incomplete params passed' do
       response = post("#{host_uri}/gather", bob: ':(')
-      expect(response.code).to eql 422
+      expect(response.status).to eql 422
     end
 
     it 'responds with uid for each request' do
-      json = JSON.dump(requests: [{ url: 'http://blah.blah/success' }])
-      response = post("#{host_uri}/gather", json: json)
-      expect(response.body['requests'].first['uid']).to_not be_empty
+      params = { requests: [{ url: 'http://blah.blah/success' }] }
+      response = post("#{host_uri}/gather", params)
+      expect(JSON.parse(response.body)['requests'].first['uid'])
+        .to_not be_empty
     end
 
     it 'responds with a hash that respects url, query string and headers' do
@@ -35,10 +34,11 @@ describe WebFetch::Server do
                   headers: { 'Content-Type' => 'whatever' } }
 
       responses = [params1, params2, params3].map do |params|
-        json = JSON.dump(requests: [params])
-        post("#{host_uri}/gather", json: json)
+        post("#{host_uri}/gather", requests: [params])
       end
-      hashes = responses.map { |res| res.body['requests'].first['hash'] }
+      hashes = responses.map do |res|
+        JSON.parse(res.body)['requests'].first['hash']
+      end
       expect(hashes.uniq.length).to eql 3
     end
   end
@@ -46,33 +46,32 @@ describe WebFetch::Server do
   describe '#gather' do
     it 'respects a given url' do
       stub = stub_request(:any, 'http://blah.blah/success')
-      json = JSON.dump(requests: [{ url: 'http://blah.blah/success' }])
-      post("#{host_uri}/gather", json: json)
+      params = { requests: [{ url: 'http://blah.blah/success' }] }
+      post("#{host_uri}/gather", params)
       expect(stub).to have_been_requested
     end
 
     it 'respects given query parameters' do
       stub = stub_request(:any, 'http://blah.blah/success?a=1')
-      json = JSON.dump(requests: [{ url: 'http://blah.blah/success?a=1' }])
-      post("#{host_uri}/gather", json: json)
+      params = { requests: [{ url: 'http://blah.blah/success?a=1' }] }
+      post("#{host_uri}/gather", params)
       expect(stub).to have_been_requested
     end
 
     it 'respects given headers' do
       stub = stub_request(:any, 'http://blah.blah/success')
              .with(headers: { 'Content-Type' => 'whatever' })
-      params = { url: 'http://blah.blah/success',
-                 headers: { 'Content-Type' => 'whatever' } }
-      json = JSON.dump(requests: [params])
-      post("#{host_uri}/gather", json: json)
+      request = { url: 'http://blah.blah/success',
+                  headers: { 'Content-Type' => 'whatever' } }
+      post("#{host_uri}/gather", requests: [request])
       expect(stub).to have_been_requested
     end
 
     it 'respects given http method' do
       stub = stub_request(:post, 'http://blah.blah/success?a=1')
-      json = JSON.dump(requests: [{ url: 'http://blah.blah/success?a=1',
-                                    method: 'POST' }])
-      post("#{host_uri}/gather", json: json)
+      params = { requests: [{ url: 'http://blah.blah/success?a=1',
+                              method: 'POST' }] }
+      post("#{host_uri}/gather", params)
       expect(stub).to have_been_requested
     end
   end
@@ -80,12 +79,16 @@ describe WebFetch::Server do
   private
 
   def get(uri)
-    Unirest.get(uri)
+    Faraday.get(uri)
   end
 
-  def post(uri, data, json = true)
-    headers = { 'Accept' => 'application/json' } if json
-    Unirest.post(uri, headers: headers,
-                      parameters: data)
+  def post(uri, params)
+    parsed = URI.parse(uri)
+    base_uri = "#{parsed.scheme}://#{parsed.host}:#{parsed.port}"
+    conn = Faraday.new(url: base_uri)
+    conn.post do |request|
+      request.url parsed.path
+      request.body = params.to_json
+    end
   end
 end
